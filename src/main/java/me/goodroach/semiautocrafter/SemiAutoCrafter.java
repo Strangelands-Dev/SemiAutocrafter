6345f5ec2e9e879f607baa0582a1ee4d7642d63e
SirVentox <joshuaolk1106@gmail.com>
2022-08-10 22:15:52 +0200

    Version 1.0.1 fix

--- src/main/java/me/goodroach/semiautocrafter/SemiAutoCrafter.java
+++ src/main/java/me/goodroach/semiautocrafter/SemiAutoCrafter.java
@@ -1,17 +1,24 @@
 package me.goodroach.semiautocrafter;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 
 public final class SemiAutoCrafter extends JavaPlugin {
 
     public static SemiAutoCrafter instance;
-    public static HashMap<String, AutocraftRecipe> Recipes = new HashMap<>();
+    public static HashMap<String, AutocraftRecipe> recipes = new HashMap<>();
 
     @Override
     public void onEnable() {
@@ -44,7 +51,7 @@ public final class SemiAutoCrafter extends JavaPlugin {
                 input.put(Material.getMaterial(a[0].toUpperCase()),Integer.parseInt(a[1]));
             }
             AutocraftRecipe recipe = new AutocraftRecipe(x,input,output,amount);
-            Recipes.put(x.toLowerCase(Locale.ENGLISH), recipe);
+            recipes.put(x.toLowerCase(Locale.ENGLISH), recipe);
             System.out.println("The plugin has successfully registered a recipe.");
         }
     }
